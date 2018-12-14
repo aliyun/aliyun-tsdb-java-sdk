1aca44066ee10854d40027f4b12acb0166432dba
hujianhong <jianhong.hjh@alibaba-inc.com>
2018-12-14 14:29:53 +0800

    add query timerang validate and supported tagv suggest

--- src/main/java/com/aliyun/hitsdb/client/value/type/Suggest.java
+++ src/main/java/com/aliyun/hitsdb/client/value/type/Suggest.java
@@ -1,7 +1,7 @@
 package com.aliyun.hitsdb.client.value.type;
 
 public enum Suggest {
-    Metrics("metrics"), Tagk("tagk");
+    Metrics("metrics"), Tagk("tagk"), Tagv("tagv");
 
     private String name;
 
